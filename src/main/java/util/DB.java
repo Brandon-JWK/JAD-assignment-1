6283fcb9ac6feee755c5211cd03ac2329300bae2
Brandon-JWK <Brandonkwan.24@ichat.sp.edu.sg>
2025-11-28 08:12:36 +0800

    Done

--- src/main/java/util/DB.java
+++ src/main/java/util/DB.java
@@ -8,13 +8,13 @@ public class DB {
 	public static Connection getConnection() throws SQLException {
 		String url = System.getenv("DB_URL");
 		String user = System.getenv("DB_USER");
-		String db = System.getenv("DB_PASSWORD");
+		String password = System.getenv("DB_PASSWORD");
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		return DriverManager.getConnection(url, user, db);
+		return DriverManager.getConnection(url, user, password);
 	}
 }
 
